0b1d4a452413da155e2b326e36e2192fedc10e4e
akpaev.e <akpaevj@gmail.com>
2022-03-30 15:40:36 +0300

    Added Group-TechLog cmdlet

---
+++ GroupTechLogCmdletCommand.ps1
@@ -0,0 +1,63 @@
+function Group-TechLog {
+    [CmdletBinding()]
+    [OutputType([PSCustomObject])]
+    Param (
+        [Parameter(Mandatory=$true,
+                   Position = 0,
+                   ValueFromPipeline=$true)]
+        [psobject]$InputObject,
+
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true)]
+        [string]$GroupProperty,
+        
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true)]
+        [string]$AggregationProperty
+    )
+
+    begin {
+        $inputObjects = New-Object 'Collections.Generic.Dictionary[string,object]'
+    }
+
+    process {
+        $group = [string]$InputObject."$GroupProperty";
+
+        if ([string]::IsNullOrEmpty($group)) { 
+            return 
+        };
+
+        $aggr = [double]$InputObject."$AggregationProperty";
+
+        if ($InputObjects.ContainsKey($group)) {
+            $a = $inputObjects[$group];
+            $a.Sum += $aggr;
+            $a.Count += 1;
+
+            if ($aggr -lt $a.Min) {
+                $a.Min = $aggr
+            }
+
+            if ($aggr -gt $a.Max) {
+                $a.Max = $aggr
+            }
+        }
+        else {
+            $inputObjects.Add($group, @{Sum = $aggr; Count = 1; Min = $aggr; Max = $aggr});
+        }
+    }
+
+    end {
+        foreach($kv in $inputObjects.GetEnumerator()) {
+            $v = $kv.Value; 
+            [PSCustomObject]@{
+                Name = $kv.Key
+                Sum = $v.Sum
+                Avg = $v.Sum / $v.Count
+                Min = $v.Min
+                Max = $v.Max
+                Count = $v.Count
+            }
+        }
+    }
+}
